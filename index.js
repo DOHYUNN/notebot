654e012548106bc89cb5b64de419c5fd3199bbad
DOHYUNN <vinke1717@gmail.com>
2021-01-16 23:33:20 +0900

    bot upload

--- index.js
+++ index.js
@@ -212,6 +212,48 @@ client.on('message', (message) => {
   }
 });
 
+ 
+let MuteRole = '채금'
+let Cooltime_Mute = 1000 //밀리세컨드 // 3초내에 칠 시 뮤트
+let User_Mute_Object = {}
+client.on('message', async message => {
+  if (message.author.bot || !message.guild) return
+  MuteRole = message.guild.roles.cache.find(r => r.id == MuteRole)
+  const M_Author = message.author
+  if (!message.member.hasPermission('ADMINISTRATOR')) {
+    let Author_Object = User_Mute_Object[M_Author.id]
+    if (!Author_Object) {
+      User_Mute_Object[M_Author.id] = {
+        time: 0,
+        interval: null,
+        muted: false
+      }
+    } else {
+      if (Author_Object.interval != null) {
+        if (Cooltime_Mute >= Author_Object.time && !Author_Object.muted) {
+          message.member.roles.add(MuteRole)
+          Author_Object.muted = true
+          
+        }
+        clearInterval(Author_Object.interval)
+        Author_Object.interval = null
+      } else if (!Author_Object.muted) {
+        Author_Object.interval = setInterval(() => {
+          Author_Object.time++
+        }, 1)
+      }
+      Author_Object.time = 0
+    }
+  }
+  if (message.member.hasPermission('ADMINISTRATOR') && /!언뮤트 <@!?(\d{17,19})>/g.test(message.content)) {
+    const Mention_member = message.mentions.members.first()
+    Mention_member.roles.remove(MuteRole)
+    User_Mute_Object[Mention_member.id].muted = false
+    User_Mute_Object[Mention_member.id].time = 0
+    message.channel.send({Mention_member}, 해방됨)
+  }
+})
+ 
 function checkPermission(message) {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) {
     message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
